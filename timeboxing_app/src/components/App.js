1bcdf5ef7804edb4d5e17adb2757ea45a11f0144
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-01-21 14:58:03 +0100

    start extracting components

---
+++ timeboxing_app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TimeboxList, EditableTimebox } from "./remainingComponents";
+
+function App() {
+  return (
+    <div className="App">
+      <TimeboxList />
+      <EditableTimebox />
+    </div>
+  );
+}
+
+export default App;
