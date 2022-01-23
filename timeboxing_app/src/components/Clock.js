b1086dd0123ece86e67e59a08b24d6c686a18813
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-01-23 19:36:17 +0100

    add homework - add BEM modifier to ProgressBar and Clock components

--- timeboxing_app/src/components/Clock.js
+++ timeboxing_app/src/components/Clock.js
@@ -3,7 +3,10 @@ import React from "react";
 function Clock({ className = "", minutes = 20, seconds = 48 }) {
   return (
     <h2 className={"Clock " + className}>
-      Pozostało {minutes}:{seconds}{" "}
+      <div className="clock__left">Pozostało: </div>
+      <div className="clock__minutes">{minutes}</div>
+      <div className="clock__separator">:</div>
+      <div className="clock__seconds">{seconds}</div>
     </h2>
   );
 }
--- timeboxing_app/src/components/ProgressBar.js
+++ timeboxing_app/src/components/ProgressBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function ProgressBar({ className = "", percent = 33 }) {
   return (
-    <div className={"progress progress--big progress--color-red" + className}>
-      <div className="progress__bar" style={{ width: `${percent}%` }}></div>
+    <div className={"progress progress--big progress--color--red" + className}>
+      <div
+        className="progress__bar progress__bar--green"
+        style={{ width: `${percent}%` }}
+      ></div>
     </div>
   );
 }
