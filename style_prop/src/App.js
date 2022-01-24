3ee1ea158079616db512b5d14a1ab4ecc2520497
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-01-24 18:28:56 +0100

    Add style_prop react app

---
+++ style_prop/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button style={{ "font-size": "20px" }}>Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
