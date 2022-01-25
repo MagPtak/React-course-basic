63368fb9228108b8941a02282bf54a7acded564d
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-01-25 12:45:35 +0100

    add styles for component with style property

--- style_prop/src/App.js
+++ style_prop/src/App.js
@@ -1,13 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button style={{ "font-size": "20px" }}>Click Me</button>
+        <Button type="danger">Click Me</Button>
+        <Button type="primary">Click Me</Button>
+        <Button>Click Me</Button>
       </header>
     </div>
+  );
+}
+
+function Button(props) {
+  const buttonStyle = {};
+  if (props.type === "primary") {
+    buttonStyle["--normal-background"] = "blue";
+    buttonStyle["--hover-background"] = "darkblue";
+    buttonStyle["--active-background"] = "lightblue";
+  } else if (props.type === "danger") {
+    buttonStyle["--normal-background"] = "red";
+    buttonStyle["--hover-background"] = "darkred";
+    buttonStyle["--active-background"] = "pink";
+  }
+  buttonStyle["--border-radius"] = "5px";
+  return (
+    <button className="Button" style={buttonStyle}>
+      {props.children}
+    </button>
   );
 }
 
