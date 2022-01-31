5102c140ffdce9ccce58d3a3130a8ad5ea5f0c42
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-01-31 16:03:24 +0100

    Add articles with mui

--- trial_app_mui/src/App.js
+++ trial_app_mui/src/App.js
@@ -12,35 +12,71 @@ import {
   Avatar,
   CardMedia,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Box, spacing } from "@mui/system";
+
+const useStyles = makeStyles({
+  media: {
+    height: 500,
+  },
+  readMore: {
+    marginLeft: "auto",
+  },
+  avatar: {
+    backgroundColor: "pink",
+  },
+  content: {
+    marginTop: 25,
+    marginBottom: 25,
+  },
+});
+
+function ArticleCard(props) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.content}>
+      <CardHeader
+        avatar={<Avatar className={classes.avatar}>JD</Avatar>}
+        title="Jane Doe"
+        subheader="Joined 2022-01-27"
+      />
+      <CardMedia
+        className={classes.media}
+        image="https://picsum.photos/id/1006/600/400"
+      />
+      <CardContent>
+        <Typography variant="h6">Clickbait title!</Typography>
+        <Typography variant="body2">Really interesting ...</Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+          <Icon color="success">favorite</Icon>
+        </IconButton>
+        <IconButton>
+          <Icon color="success">share</Icon>
+        </IconButton>
+        <Box sx={{ ml: "auto" }}>
+          <Button
+            className={classes.readMore}
+            variant="outlined"
+            color="success"
+          >
+            Read more
+          </Button>
+        </Box>
+      </CardActions>
+    </Card>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <CssBaseline>
         <Container>
-          <Card>
-            <CardHeader
-              avatar={<Avatar>JD</Avatar>}
-              title="Jane Doe"
-              subheader="Joined 2022-01-27"
-            />
-            <CardMedia image="https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g" />
-            <CardContent>
-              <Typography variant="h6">Clickbait title!</Typography>
-              <Typography variant="body2">Really interesting ...</Typography>
-            </CardContent>
-            <CardActions>
-              <IconButton>
-                <Icon color="success">favorite</Icon>
-              </IconButton>
-              <IconButton>
-                <Icon color="success">share</Icon>
-              </IconButton>
-              <Button variant="outlined" color="success">
-                Read more
-              </Button>
-            </CardActions>
-          </Card>
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
         </Container>
       </CssBaseline>
     </div>
